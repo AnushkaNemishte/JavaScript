69b5cfe89d65d8c5940893f0d65e879d9c10bf0a
Anushka <anushka.nemishte@chitaledairy.com>
2023-07-29 15:25:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+//Data Types
+//Primitive datatypes & Non-Primitive Data types
+//Primitive datatypes are -:
+//1)String   3)Boolean    5)Null     7)Symbol
+//2)Number   4)Undefined  6)BigInt
+//Non-Primitive Datatypes are -:
+//objects
+
+//1)String
+const namee = "Anushka";
+console.log(namee);
+const lang = "JavaScript";
+console.log(lang);
+const channel = "Code-evolution";
+console.log(channel);
+
+//2)Number
+const total = 10;
+console.log(total);
+const PI = 3.14;
+console.log(PI);
+
+//3)Boolean
+const newUser = true;
+console.log(newUser);
+const oldUser = false;
+console.log(oldUser);
+
+//4)Undefined
+let result;
+console.log(result);
+const res = undefined;
+console.log(res);
+
+//5)Null
+const noUser = null;
+console.log(noUser);
+
+//Objects non primitive datatypes
+const person = {
+  firstName: "Anushka",
+  lastName: "Nemishte",
+  age: 21,
+};
+console.log("Information", person.age);
+
+//Array
+const oddNumbers = [1, 3, 5, 9, 11];
+console.log(oddNumbers[2]);
---
+++ operators.js
@@ -0,0 +1,34 @@
+//An operator is a special symbol used to perform operations on values and variables
+//1)Assignment Operators       2)Arithmetic Operators
+//3)Comparison Operators       4)Logical Operators
+//5)String Operators
+
+//1)Assignment Operators
+//Assignment operators assign values to JavaScript variables.
+let namee = "Anushka Nemishte";
+console.log(namee);
+
+//2)Arithmetic Operators
+//Arithmetic Operators are used to perform arithmetic on numbers:
+let x = 5;
+let y = 10;
+console.log("Addition = ", x + y);
+console.log("Substration = ", x - y);
+console.log("Multiplication = ", x * y);
+console.log("Division = ", x / y);
+
+//3)Comparison Operators
+//Comparison operators are used in logical statements to determine equality or difference between variables or values.
+let a = 5;
+let b = 6;
+console.log("Equal", a == b);
+console.log("Not Equal", a != b);
+console.log("Greater than ", a > b);
+console.log("Smaller than ", a < b);
+
+//4)Logical Operators
+//Logical operators are used to determine the logic between variables or values.
+let p = 6;
+let q = 3;
+console.log("Logical Add(&&) Operator", p > q && 3 < 6); //&& operator gives true only if both conditions are true
+console.log("Logical OR(||) Operator", p > q || p < q); //|| operator gives true when either 1 condition is also true
