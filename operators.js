1a4fb4aa048f65536a3b0d0fc3f401cbec9c12fe
Anushka <anushka.nemishte@chitaledairy.com>
2023-07-29 15:59:10 +0530

    initial commit

--- operators.js
+++ operators.js
@@ -33,3 +33,25 @@ let q = 3;
 console.log("Logical Add(&&) Operator", p > q && 3 < 6); //&& operator gives true only if both conditions are true
 console.log("Logical OR(||) Operator", p > q || p < q); //|| operator gives true when either 1 condition is also true
 console.log("Logical NOT(!) Operator", p != q);
+
+//5)String Operators
+//These operators allow you to join one string to another easily. These operators are called “concatenate” and “concatenate assignment“. 
+let firstName = "Anushka";
+let lastName = "Nemishte";
+console.log("FullName = ",firstName +" "+lastName);
+
+//6)Ternary Operators
+//The ternary operator is a simplified conditional operator like if / else.
+const isEven = 10%2===0?"Even number":"Odd number";
+console.log(isEven)
+
+//Basic ternary operator to determine the larger of two numbers
+const num1 = 45;
+const num2 = 35;
+const largerNumber = num1>num2 ? num1:num2
+console.log("Large number is ",largerNumber)
+
+//Ternary operator for checking if a number is even or odd
+const num = 15
+const isEvenOrOdd = num%2===0 ? "Even":"Odd";
+console.log(`The number ${num} is ${isEvenOrOdd}`);
