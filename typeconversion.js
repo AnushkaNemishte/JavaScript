ca5b69828a43d5ba5c44e1fce6ad00726e83b691
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-07-29 20:16:33 +0530

    initial code

--- typeconversion.js
+++ typeconversion.js
@@ -1,2 +1,3 @@
 //Implicit conversion occurs when JavaScript automatically converts one data type to another without any explicit instructions from the programmer.
 //Explicit conversion occurs when the programmer manually converts one data type to another using built-in functions or type conversion methods.
+console.log("Hello world")
