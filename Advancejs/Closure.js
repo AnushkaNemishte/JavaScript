609143ebff3322ef0e5179a0967c8c332d9aaa2c
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-08-02 21:44:50 +0530

    Javascript

---
+++ Advancejs/Closure.js
@@ -0,0 +1,2 @@
+//Closure is the combination of a function bundled together with references to its surrounding state.Closures are created every time a function is created,at function creation time
+//It can do things like hold variables out of scope, or remember arguments across calls.
---
+++ Advancejs/Scope.js
@@ -0,0 +1,3 @@
+//block Scope - variables declared inside a pair of curly braces cannot be accessed from outside the block.
+//Function scope - variables declared inside a function cannot not be accessed from outside the function
+//Global Scope - globally scoped variables can be accessed inside a block or function 
