2cd06518843ab8f408519d2b4ca3e2af2fbc7a01
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-08-30 20:26:08 +0530

    initial commit

---
+++ DOM.js
@@ -0,0 +1,11 @@
+//Single Element selector
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('.container'));
+
+//Multiple element selector
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
+const ul =document.querySelector('.items')
+ul.remove()
